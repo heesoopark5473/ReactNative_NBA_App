2d95c0889203517f8ac5c0b4aa075a71a7f540af
hep656 <hep656@usask.ca>
2021-05-10 03:14:50 -0600

    gameSchedule

--- app/components/games/index.js
+++ app/components/games/index.js
@@ -19,21 +19,91 @@ class GameComponent extends Component {
         this.props.dispatch(getGames());
     }
 
+    showGames = (list) => (
+        list.games ?
+            list.games.map((item, i)=>(
+                <TouchableOpacity
+                    onPress={()=> this.props.navigation.navigate('Article', {
+                        ...item
+                    })}
+                    key={i}
+                >
+                    <View style={styles.gameContainer}>
+                        <View style={styles.gamebox}>
+                            <Image
+                                source={{uri:`${item.awayData.logo}`}}
+                                style={{height:80, width:80}}
+                                resizeMode="contain"
+                            />
+                            <Text style={styles.teamRecord}>{item.awayData.wins} - {item.awayData.loss}</Text>
+                        </View>
+                        <View style={styles.gamebox}>
+                            <Text style={styles.gameTime}>{item.time}</Text>
+                            <Text>{Moment(item.date).format('d MMMM')}</Text>
+                        </View>
+                        <View style={styles.gamebox}>
+                            <Image
+                                source={{uri:`${item.localData.logo}`}}
+                                style={{height:80, width:80}}
+                                resizeMode="contain"
+                            />
+                            <Text style={styles.teamRecord}>{item.localData.wins} - {item.localData.loss}</Text>
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            ))
+        :null
+    )
+
     render() {
         return (
-            <View>
-                <Text>Hello I am the Game</Text>
-            </View>
+            <ScrollView style={{backgroundColor: '#f0f0f0'}}>
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    flexWrap: 'nowrap'
+                }}>
+                    { this.showGames(this.props.Games) }
+
+                </View>
+
+            </ScrollView>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    gameContainer: {
+        flexDirection: 'row',
+        marginBottom: 10,
+        backgroundColor: '#fff',
+        shadowColor: '#dddddd',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 1,
+        borderRadius: 2,
+    },
+    gamebox: {
+        width: '33.3%',
+        height: 100,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    teamRecord: {
+        // fontFamily: "Roboto-Light,"
+        fontSize: 12
+    },
+    gameTime: {
+        // fontFamily: "Roboto-Bold",
+        fontSize: 15
+    }
 
 });
 
 function mapStateToProps(state) {
-    console.log(JSON.stringify(state,null,2));
+    // console.log(JSON.stringify(state,null,2));
     return {
         Games: state.Games
     }
